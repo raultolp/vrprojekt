1b8da1b869b7e37a909bbdd0a3b6fe9d5692366c
raultolp <raul.tolp@gmail.com>
2018-09-12 08:56:26 +0300

    Initial commit

---
+++ src/main/java/ee/ut/cs/wad2018/vrprojekt/VrprojektApplication.java
@@ -0,0 +1,12 @@
+package ee.ut.cs.wad2018.vrprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrprojektApplication.class, args);
+	}
+}
---
+++ src/test/java/ee/ut/cs/wad2018/vrprojekt/VrprojektApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.ut.cs.wad2018.vrprojekt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VrprojektApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
